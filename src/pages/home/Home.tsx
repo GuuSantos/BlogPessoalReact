7f73517c50e702debfa925f521faec9567d5563a
Gustavo <gustavossantos41@outlook.com>
2022-04-25 11:39:53 -0300

    Criando a page Home

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://storage.googleapis.com/atados-v3/user-uploaded/images/8010b1b6-32bc-4dd4-bc11-a4a607c9974c.png" alt="Imagem Tela Inicial" className="img" />
+        </>
+    );
+}
+
+export default Home;
